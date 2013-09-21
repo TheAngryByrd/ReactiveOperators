be7d66a2fcade94505ebaa979a31a84298652549
Jimmy Byrd <jimmybyrd@gmail.com>
2013-09-21 02:58:32 -0400

    Added operators

---
+++ ReactiveOperators/ReactiveOperators/ReactiveBooleanOperators.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ReactiveOperators
+{
+    public static class ReactiveBooleanOperators
+    {
+        public static IObservable<bool> Not(this IObservable<bool> operand)
+        {
+            return operand.Select(x => !x);
+        }
+
+        public static IObservable<bool> And(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x,y) => x && y));
+        }
+        
+        public static IObservable<bool> Nand(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x,y) => (x && y))).Not();
+        }
+
+        public static IObservable<bool> Or(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x,y) => x || y));
+        }
+
+        public static IObservable<bool> Nor(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x,y) => x || y)).Not();
+        }
+
+        public static IObservable<bool> Xor(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x, y) => x ^ y));
+        }
+        
+        public static IObservable<bool> Xnor(this IObservable<bool> operand, params IObservable<bool>[] operands)
+        {
+            return operands.Aggregate(operand, (current, observable) => current.CombineLatest(observable, (x, y) => x ^ y)).Not();
+        }      
+
+
+
+        
+    }
+}
