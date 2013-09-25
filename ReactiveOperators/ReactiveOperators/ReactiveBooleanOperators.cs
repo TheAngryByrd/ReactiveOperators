1a3cd503504331a4d6c0afe554c57e9454fcb8b3
Jimmy Byrd <jimmybyrd@gmail.com>
2013-09-24 21:22:06 -0400

    Added doc comments

--- ReactiveOperators/ReactiveOperators/ReactiveBooleanOperators.cs
+++ ReactiveOperators/ReactiveOperators/ReactiveBooleanOperators.cs
@@ -1,21 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 
 namespace ReactiveOperators
 {
     public static class ReactiveBooleanOperators
     {
+        /// <summary>
+        /// Projects a boolean obeservable into its opposite form.
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <returns></returns>
         public static IObservable<bool> Not(this IObservable<bool> operand)
         {
             return operand.Select(x => !x);
         }
 
+        /// <summary>
+        /// Combines the latest value from boolean observables with the And operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> And(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return PerformOperation(And, operand, operands);
         }
         
+        /// <summary>
+        /// Combines the latest value from boolean observables with the Nand operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> Nand(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return AntiPerformOperation(And, operand, operands);
@@ -26,11 +45,23 @@ namespace ReactiveOperators
             return x && y;
         }
 
+        /// <summary>
+        /// Combines the latest value from boolean observables with the Or operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> Or(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return PerformOperation(Or, operand, operands);
         }
 
+        /// <summary>
+        /// Combines the latest value from boolean observables with the Nor operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> Nor(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return AntiPerformOperation(Or, operand, operands);
@@ -41,11 +72,23 @@ namespace ReactiveOperators
             return x || y;
         }
 
+        /// <summary>
+        /// Combines the latest value from boolean observables with the Xor operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> Xor(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return PerformOperation(Xor, operand, operands);
         }
         
+        /// <summary>
+        /// Combines the latest value from boolean observables with the Xnor operator
+        /// </summary>
+        /// <param name="operand"></param>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static IObservable<bool> Xnor(this IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return AntiPerformOperation(Xor, operand, operands);
@@ -64,6 +107,8 @@ namespace ReactiveOperators
         private static IObservable<bool> AntiPerformOperation(Func<bool, bool, bool> @operator, IObservable<bool> operand, params IObservable<bool>[] operands)
         {
             return PerformOperation(@operator, operand, operands).Not();
+
+            
         }
-    }
+    }  
 }
